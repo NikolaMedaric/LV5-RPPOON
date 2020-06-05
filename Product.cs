a959ee5b40ce3917e794d784ebf796386e692b27
NikolaMedaric <63855981+NikolaMedaric@users.noreply.github.com>
2020-06-05 18:29:49 +0200

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV5
+{
+    class Product : IShipable
+    {
+        private double price;
+        private double weight;
+        private string description;
+
+        public Product(double price, double weight, string description)
+        {
+            this.price = price;
+            this.weight = weight;
+            this.description = description;
+        }
+        public double Price { get { return this.price; } }
+        public double Weight { get { return this.weight; } }
+        public string Description(int depth = 0)
+        {
+            return new string(' ', depth) + this.description;
+        }
+    }
+}
---
+++ ShippingServise.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV5
+{
+    class ShippingServise 
+    {
+        private double unitPrice;
+
+        public ShippingServise(double unitPrice)
+        {
+            this.unitPrice = unitPrice;
+        }
+
+        public double UnitPrice { get { return this.unitPrice; } }
+
+        public double DeliveryPrice(double weight, double unitPrice)
+        {
+            return weight * unitPrice;             
+        }
+    }
+}
